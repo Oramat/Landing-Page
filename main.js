6b74f5c9d7f0458dd347372c916cbf3d2c85eb18
Oramat <maxweloramat@gmail.com>
2024-02-22 17:06:36 +0300

    Landing page

---
+++ main.js
@@ -0,0 +1,8 @@
+function openNavBar(){
+    document.getElementById("sideNavigationBar")
+    .style.width="50%";
+}
+function closeNavbar(){
+    document.getElementById("sideNavigationBar")
+    .style.width="0%";
+}
